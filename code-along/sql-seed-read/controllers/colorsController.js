0e61af72648fa1d89c1db9154e29cbab1e989acb
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-17 20:31:50 -0400

    show and create complete

--- code-along/sql-seed-read/controllers/colorsController.js
+++ code-along/sql-seed-read/controllers/colorsController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { getAllColors } = require("../queries/color");
+const { getAllColors, getColor, createColor } = require("../queries/color");
+const { checkName, checkBoolean } = require("../validations/checkColors.js");
 
 router.get("/", async (req, res) => {
   const allColors = await getAllColors();
@@ -9,6 +10,21 @@ router.get("/", async (req, res) => {
   } else {
     res.status(500).json({ error: "server error" });
   }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const color = await getColor(id);
+  if (color) {
+    res.json(color);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+router.post("/", checkName, checkBoolean, async (req, res) => {
+  const color = await createColor(req.body);
+  res.json(color);
 });
 
 module.exports = router;
--- code-along/sql-seed-read/queries/color.js
+++ code-along/sql-seed-read/queries/color.js
@@ -9,4 +9,26 @@ const getAllColors = async () => {
   }
 };
 
-module.exports = { getAllColors };
+const getColor = async (id) => {
+  try {
+    const oneColor = await db.one("SELECT * FROM colors WHERE id = $1", id);
+    return oneColor;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createColor = async (color) => {
+  const { name, is_favorite } = color;
+  try {
+    const currColor = await db.one(
+      "INSERT INTO colors (name, is_favorite) VALUES ($1, $2) RETURNING *",
+      [name, is_favorite]
+    );
+    return currColor;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllColors, getColor, createColor };
